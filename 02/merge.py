adfdfd7bae6f27024aa20ab3e7ddde8a10224ce9
oreqizer <oreqizer@gmail.com>
2020-10-24 15:45:23 +0200

    02/merge

--- 02/merge.py
+++ 02/merge.py
@@ -23,59 +23,81 @@
 #
 # Do not modify the input dictionary.
 
-def merge_dict(dict_in, equiv, combine):
-    pass
-
 import copy
+
+
+def merge_dict(dict_in, equiv, combine):
+    res = {}
+
+    keys_left = [k for k in dict_in.keys() if k not in [e for eq in equiv for e in eq]]
+    for k in keys_left:
+        res[k] = dict_in[k]
+    
+    for eq in equiv:
+        vals = []
+        key_val = None
+        for key in eq:
+            if key not in dict_in:
+                continue
+
+            # set proper key
+            if key_val == None or key < key_val:
+                key_val = key
+
+            vals.append(dict_in[key])
+        
+        res[key_val] = combine(vals)
+
+    return res
+
 
 def test_main():
 
-    combine = lambda x : sum( x )
-    dict_in = { 1: 1, 2: 2, 3: 3, 4: 4, 5: 1, 6: 2, 7: 3  }
-    eq = [ set( [ 1, 3, 5, 7 ] ), set( [ 2, 4, 6 ] ) ]
+    def combine(x): return sum(x)
+    dict_in = {1: 1, 2: 2, 3: 3, 4: 4, 5: 1, 6: 2, 7: 3}
+    eq = [set([1, 3, 5, 7]), set([2, 4, 6])]
 
     dict_orig = dict_in.copy()
-    assert merge_dict( dict_in, eq, combine ) == { 1: 8, 2: 8 }
+    assert merge_dict(dict_in, eq, combine) == {1: 8, 2: 8}
     assert dict_in == dict_orig
 
-
-    combine = lambda x : sum( [ len( s ) for s in x ] )
-    dict_in = { 2: 'two', 3: 'three', 6: 'two', 1: 'one', 9: 'woo' }
-    eq = [ set( [ 2 ] ), set( [ 3, 6, 1 ] ) ]
+    def combine(x): return sum([len(s) for s in x])
+    dict_in = {2: 'two', 3: 'three', 6: 'two', 1: 'one', 9: 'woo'}
+    eq = [set([2]), set([3, 6, 1])]
 
     dict_orig = dict_in.copy()
-    assert merge_dict( dict_in, eq, combine ) == { 2: 3, 1: 11, 9: 'woo' }
+    assert merge_dict(dict_in, eq, combine) == {2: 3, 1: 11, 9: 'woo'}
     assert dict_in == dict_orig
 
-
-    combine = lambda x : sum( x )
-    dict_in = { 1: 9, 8: "eek", "ef": 22 }
+    def combine(x): return sum(x)
+    dict_in = {1: 9, 8: "eek", "ef": 22}
     eq = []
 
     dict_orig = dict_in.copy()
-    assert merge_dict( dict_in, eq, combine ) == dict_orig
+    assert merge_dict(dict_in, eq, combine) == dict_orig
     assert dict_in == dict_orig
 
+    dict_in = {"ab": {7: 33, 9: 1, 13: 45}, "abcde": {3: 9, 0: 5, -1: 4},
+               "foo": {1: 3, 91: 3, 4: 3, 5: -1, 8: 4}, "val": {6: 7}}
+    eq_out = [set(["ab", "abcde", "val"])]
+    eq_in = [set([1, 91, 8, 6]), set([3, 7, 9, -1]), set([0])]
 
-    dict_in = { "ab": { 7: 33, 9: 1, 13: 45 }, "abcde": { 3: 9, 0: 5, -1: 4 },
-                "foo": { 1: 3, 91: 3, 4: 3, 5: -1, 8: 4 }, "val": { 6: 7 } }
-    eq_out = [ set( [ "ab", "abcde", "val" ] ) ]
-    eq_in = [ set( [ 1, 91, 8, 6 ] ), set( [ 3, 7, 9, -1 ] ), set( [ 0 ] ) ]
-
-    dict_orig = copy.deepcopy( dict_in )
+    dict_orig = copy.deepcopy(dict_in)
 
     # list of dictionaries into one dictionary
-    def flatten( x ):
+    def flatten(x):
         d = {}
         for dic in x:
-            d.update( dic )
+            d.update(dic)
         return d
 
-    combine = lambda x : merge_dict( flatten( x ), eq_in, lambda y : sum( y ) )
+    def combine(x): return merge_dict(flatten(x), eq_in, lambda y: sum(y))
 
-    res = { "ab": { 13: 45, -1: 47, 0: 5, 6: 7 }, "foo": { 1: 3, 91: 3, 4: 3, 5: -1, 8: 4 } }
-    assert merge_dict( dict_in, eq_out, combine ) == res
+    res = {"ab": {13: 45, -1: 47, 0: 5, 6: 7},
+           "foo": {1: 3, 91: 3, 4: 3, 5: -1, 8: 4}}
+    assert merge_dict(dict_in, eq_out, combine) == res
     assert dict_in == dict_orig
+
 
 if __name__ == "__main__":
     test_main()
