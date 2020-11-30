cc4fcac8d80b0af378109dbb9968107c9f85dfbc
oreqizer <oreqizer@gmail.com>
2020-11-30 15:17:23 +0100

    hw3

--- hw3/classes.py
+++ hw3/classes.py
@@ -1,1 +1,140 @@
 # Additional types (classes) that you may want to define.
+class Atom:
+    def __init__(self, value):
+        self.value = value
+
+    def __eq__(self, o):
+        return self.value == o.value
+
+    def __str__(self):
+        return str(self.value)
+
+    def is_atom(self):
+        return True
+
+    def is_boolean(self):
+        return isinstance(self, Boolean)
+
+    def is_compound(self):
+        return isinstance(self, Compound)
+
+    def is_identifier(self):
+        return isinstance(self, Identifier)
+
+    def is_literal(self):
+        return isinstance(self, Literal)
+
+    def is_number(self):
+        return isinstance(self, Number)
+
+    def is_string(self):
+        return isinstance(self, String)
+
+
+class Literal(Atom):
+    def __init__(self, value):
+        super().__init__(value)
+
+
+class Boolean(Literal):
+    def __init__(self, value):
+        super().__init__(value)
+
+    def __str__(self):
+        return '#t' if self.value else '#f'
+
+    def __bool__(self):
+        return self.value
+
+
+class Compound(Atom):
+    def __init__(self, values: list):
+        super().__init__(values)
+
+    def __eq__(self, o):
+        return str(self) == str(o)
+
+    def __iter__(self):
+        self.number = 0
+        return self
+
+    def __next__(self):
+        if self.number == len(self.value):
+            raise StopIteration
+        
+        res = self.value[self.number]
+        self.number += 1
+
+        return res
+
+    def __str__(self):
+        return f'({" ".join([str(x) for x in self.value])})'
+
+    def is_atom(self):
+        return False
+
+
+class Identifier(Atom):
+    def __init__(self, value):
+        super().__init__(value)
+
+
+class Number(Literal):
+    def __init__(self, value):
+        super().__init__(value)
+
+    def do(self, o, fn):
+        return fn(self.value, o.value if isinstance(o, Number) else o)
+
+    def __int__(self):
+        return int(self.value)
+
+    def __float__(self):
+        return float(self.value)
+
+    def __eq__(self, o):
+        return self.do(o, lambda a, b: a == b)
+
+    def __lt__(self, o):
+        return self.do(o, lambda a, b: a < b)
+
+    def __gt__(self, o):
+        return self.do(o, lambda a, b: a > b)
+
+    def __add__(self, o):
+        return self.do(o, lambda a, b: a + b)
+
+    def __radd__(self, o):
+        return self.do(o, lambda a, b: b + a)
+
+    def __sub__(self, o):
+        return self.do(o, lambda a, b: a - b)
+
+    def __rsub__(self, o):
+        return self.do(o, lambda a, b: b - a)
+
+    def __mul__(self, o):
+        return self.do(o, lambda a, b: a * b)
+
+    def __rmul__(self, o):
+        return self.do(o, lambda a, b: b * a)
+
+    def __floordiv__(self, o):
+        return self.do(o, lambda a, b: a // b)
+
+    def __rfloordiv__(self, o):
+        return self.do(o, lambda a, b: b // a)
+
+    def __truediv__(self, o):
+        return self.do(o, lambda a, b: a / b)
+
+    def __rtruediv__(self, o):
+        return self.do(o, lambda a, b: b / a)
+
+
+class String(Literal):
+    def __init__(self, value):
+        super().__init__(value)
+
+    def __str__(self):
+        return f'"{self.value}"'
--- hw3/lisp.py
+++ hw3/lisp.py
@@ -1,3 +1,163 @@
 # Implement ‹parse› here. You can define helper classes either here
 # or in ‹classes.py› (the latter will not be directly imported by
 # the tests).
+import re
+
+from classes import Atom, Boolean, String, Number, Identifier, Compound
+
+
+def parse(s):
+    if not check_pairing(s) or s == '':
+        raise Exception
+
+    res = tokenize(s)
+    if res == '':
+        return Atom('')
+
+    if type(res) == str:
+        if len(res) >= 2 and res[0] == '"' and res[-1] == '"':
+            return String(res[1:-1])
+
+        if res == '#f':
+            return Boolean(False)
+
+        if res == '#t':
+            return Boolean(True)
+
+        num = maybe_number(res)
+        if num is not None:
+            return Number(num)
+
+        if is_identifier(res):
+            space = s.find(' ')
+            return Identifier(s[:space] if space > 0 else s)
+
+        raise Exception
+
+    return Compound([parse(x) for x in res])
+
+
+def tokenize(s):
+    pure = s.strip()
+    if len(pure) < 2 or pure[0] != '(' or pure[-1] != ')':
+        # Atom
+        return pure
+
+    expr = pure[1:-1].strip()  # Expression without ( )
+    words = []                 # Resulting words
+    word = ''                  # Current word-in-progress
+    depth = 0                  # ( ) nesting
+    escaped = False            # Escaped chars in strings
+    is_string = False          # Is the current word a string
+
+    for i, c in enumerate(expr):
+        word += c
+
+        if depth > 0:
+            if c == '(':
+                # Bracket enter
+                depth += 1
+
+            if c == ')':
+                # Bracket leave
+                depth -= 1
+                
+            if depth == 0:
+                words.append(word)
+                word = ''
+
+        elif is_string:
+            if escaped:
+                # Ignore next string char
+                escaped = False
+                continue
+
+            if c in '"':
+                # String end
+                is_string = False
+                words.append(word)
+                word = ''
+
+            escaped = c == '\\'
+
+        else:
+            if i - 1 >= 0 and expr[i - 1] == '"' and not c.isspace():
+                # Not beginning, not inside string and not space
+                raise Exception
+
+            if c == '(':
+                # Bracket enter
+                depth += 1
+
+            elif c == '"':
+                # String enter
+                is_string = True
+
+            elif len(word.strip()) > 0 and c in ' ':
+                # Whitespace, word end
+                words.append(word)
+                word = ''
+
+        if i == len(expr) - 1 and len(word) > 0:
+            # Last char and words exist
+            words.append(word)
+
+    return words
+
+
+# === UTILS ===
+
+
+def maybe_number(s):
+    try:
+        return int(s)
+    except ValueError:
+        try:
+            return float(s)
+        except:
+            return None
+
+
+def check_pairing(s):
+    depth = 0         # ( ) nesting
+    string_apos = 0   # Number of "
+    escaped = False   # Escaped chars in strings
+
+    for c in s:
+        if escaped:
+            if c not in '"\\':
+                raise Exception
+
+            escaped = False
+            continue
+
+        if c == '(':
+            depth += 1
+            continue
+
+        if c == ')':
+            depth -= 1
+            continue
+
+        if c == '"':
+            string_apos += 1
+            continue
+
+        escaped = c == '\\'
+
+    return depth == 0 and string_apos % 2 == 0
+
+
+def is_ident_char(char):
+    return char.isalpha() or char in set(['!', '$', '%', '&', '*', '/', ':', '<', '=', '>', '?', '^', '_', '~'])
+
+
+def is_identifier(expr):
+    first = expr[0]
+    if len(expr) == 1 and first in ('+', '-'):
+        return True
+
+    if is_ident_char(first):
+        return all([is_ident_char(c) or c.isnumeric() or c in set(['+', '-', '.', '@', '#']) for c in expr])
+
+    raise Exception
