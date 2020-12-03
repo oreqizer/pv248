824f44b55916c775ab53f23687d92f2f2f5f9873
oreqizer <oreqizer@gmail.com>
2020-12-03 14:23:06 +0100

    08

--- 08/tcp-echo.py
+++ 08/tcp-echo.py
@@ -1,5 +1,5 @@
 # Start a server, on localhost, on the given port (using asyncio.start_server)
-# and have two clients connect to it. 
+# and have two clients connect to it.
 # The server takes care of the underlying sockets, so we will not be creating
 # them manually. Data is, again, transferred as `bytes` object.
 
@@ -11,31 +11,55 @@
 # by the clients on the client side.
 # Make sure to close the writing side of sockets once data is exhausted.
 
+from io import StringIO
+import random
+import sys
 import asyncio
 
 # Server-side, handler for connecting clients. Read the message from
 # the client and echo it back to the client.
-async def handle_client( reader, writer ):
-    # print( "server received & sending", ... )
-    pass
+
+
+async def handle_client(reader, writer):
+    r = await reader.read(100)
+    txt = r.decode()
+    print(f"server received & sending {txt}")
+    writer.write(r)
+    writer.close()
+
 
 # Client, connect to the server, send a message, wait for the answer
 # and return this answer. Assert that the answer matches the message
 # sent. Sleep for 1 second before sending 'world', to ensure message order.
-async def client( port, msg ):
-    # print( "client sending", ... )
-    # print( "client received", ... )
-    pass
+
+
+async def client(port, msg):
+    reader, writer = await asyncio.open_connection(port=port)
+    print(f"client sending {msg}")
+    if msg == "world":
+        await asyncio.sleep(1)
+    writer.write(msg.encode())
+    data = await reader.read()
+    txt = data.decode()
+    print(f"client received {txt}")
+    writer.close()
+    return txt
 
 # Start the server and the two clients, gather the data back from the
 # two clients into a list, return this list; starting with the 'hello'
 # client. Use the provided port.
-async def main( port ):
-    pass
 
-import sys
-import random
-from io import StringIO
+
+async def main(port):
+    server = await asyncio.start_server(handle_client, port=port)
+    await server.start_serving()
+    res = await asyncio.gather(
+        client(port, "hello"),
+        client(port, "world"),
+    )
+    server.close()
+    return res
+
 
 def test_main():
 
@@ -43,19 +67,20 @@ def test_main():
     out = StringIO()
     sys.stdout = out
 
-    data = asyncio.run( main( random.randint( 9000, 13000 ) ) )
-    print( data )
+    data = asyncio.run(main(random.randint(9000, 13000)))
+    print(data)
     assert data == ['hello', 'world']
 
     sys.stdout = stdout
     output = out.getvalue()
-    print( output )
+    print(output)
     output = output.split('\n')
 
-    assert 'client sending hello' in output[ 0 : 3 ]
-    assert 'client sending world' in output[ 0 : 3 ]
-    assert 'server received & sending hello' in output[ 1 : 3 ]
-    assert 'server received & sending world' in output[ 2 : ]
+    assert 'client sending hello' in output[0: 3]
+    assert 'client sending world' in output[0: 3]
+    assert 'server received & sending hello' in output[1: 3]
+    assert 'server received & sending world' in output[2:]
+
 
 if __name__ == "__main__":
     test_main()
--- 08/unix-rot13.py
+++ 08/unix-rot13.py
@@ -8,24 +8,61 @@
 # require very small changes from your previous solution to
 # ‹tcp-echo›.
 
+import os
+from io import StringIO
+import sys
 import asyncio
 
-async def handle_client( reader, writer ):
-    # print( "server received", ... )
-    # print( "server sending", ... )
-    pass
 
-async def client( msg, path ):
-    # print( "client sending", ... )
-    # print( "client received", ... )
-    pass
+def encrypt(text, s = 13):
+    result = ""
+    # transverse the plain text
+    for i in range(len(text)):
+        char = text[i]
+        # Encrypt uppercase characters in plain text
 
-async def main( path ):
-    pass
+        if (char.isupper()):
+            result += chr((ord(char) + s-65) % 26 + 65)
+        # Encrypt lowercase characters in plain text
+        else:
+            result += chr((ord(char) + s - 97) % 26 + 97)
+    return result
 
-import sys
-from io import StringIO
-import os
+
+async def handle_client(reader, writer):
+    r = await reader.read(100)
+    txt = r.decode()
+    print(f"server received {txt}")
+    enc = encrypt(txt)
+    print(f"server sending {enc}")
+    writer.write(enc.encode())
+    writer.close()
+
+
+async def client(msg, path):
+    reader, writer = await asyncio.open_unix_connection(path=path)
+    print(f"client sending {msg}")
+    if msg == "world":
+        await asyncio.sleep(1)
+    writer.write(msg.encode())
+    data = await reader.read()
+    txt = data.decode()
+    print(f"client received {txt}")
+    writer.close()
+    return txt
+
+
+async def main(path):
+    server = await asyncio.start_unix_server(handle_client, path=path)
+    await server.start_serving()
+    res = await asyncio.gather(
+        client("hello", path),
+        client("world", path),
+    )
+    server.close()
+    os.remove(path)
+    return res
+
 
 def test_main():
 
@@ -34,23 +71,24 @@ def test_main():
     sys.stdout = out
 
     path = "./tmp.socket"
-    data = asyncio.run( main( path ) )
-    print( data )
+    data = asyncio.run(main(path))
+    print(data)
     assert data == ['uryyb', 'jbeyq']
 
     sys.stdout = stdout
     output = out.getvalue()
-    print( output )
+    print(output)
     output = output.split('\n')
 
-    assert 'client sending hello' in output[ 0 : 3 ]
-    assert 'client sending world' in output[ 0 : 3 ]
-    assert 'server received hello' in output[ 1 : 3 ]
+    assert 'client sending hello' in output[0: 3]
+    assert 'client sending world' in output[0: 3]
+    assert 'server received hello' in output[1: 3]
     assert 'server sending uryyb' in output
     assert 'server received world' in output
     assert 'server sending jbeyq' in output
 
-    assert not os.path.exists( path )
+    assert not os.path.exists(path)
+
 
 if __name__ == "__main__":
     test_main()
