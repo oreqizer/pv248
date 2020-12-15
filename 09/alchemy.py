541602c5bcc93eeee06afbb7a006961dfb68bf3d
oreqizer <oreqizer@gmail.com>
2020-12-15 21:47:35 +0100

    alchemy almost

--- 09/alchemy.py
+++ 09/alchemy.py
@@ -7,15 +7,65 @@
 #    representing a rule - the first element of the tuple being
 #    a list of outputs, the second element a list of required
 #    inputs), again with substances and quantities on each.
-# 
+#
 # The sum of the quantities on the right hand side of the list is
 # strictly less than that on the left side. Decide whether it is
 # possible to get from the available substances to the desired,
 # using the given rules: return a boolean. It does not matter
 # whether there are leftovers. Rules can be used repeatedly.
 
+
+import math
+
+
+def to_dict(l):
+    d = {}
+    for n, q in l:
+        d[n] = q
+    return d
+
+
 def alchemy(available, desired, rules):
-    pass
+    a = to_dict(available)
+
+    for desire in desired:
+        stack = [desire]  # Current needs
+        while len(stack) > 0:
+            (name, q) = stack.pop()
+
+            # Check available first
+            a_has = a.get(name, 0)
+            if a_has >= q:
+                a[name] -= q
+                continue
+
+            # Take at least part
+            if a_has > 0:
+                a[name] -= q
+                stack.append((name, q - a_has))
+                continue
+
+            # Transmute what we can
+            options = []
+            for want, res in rules:
+                for rn, rq in res:
+                    if rn == name:
+                        options.append([(wn, wq * math.ceil(q / rq))
+                                        for wn, wq in want])
+                    break
+
+            match = False
+            for want in options:
+                if alchemy([(an, aq) for an, aq in a.items()], [*stack, *want], rules):
+                    match = True
+                    for wn, wq in want:
+                        stack.append((wn, wq))
+
+            # Out of options :(
+            if not match:
+                return False
+
+    return True
 
 # The rules from tests in a readable format, for your convenience:
 #
@@ -36,86 +86,86 @@ def test_main():
     # each rule in the list is a tuple of two lists ( [ingredients], [result] )
     # each of the ingredients and products is again a tuple in the format (name, amount)
 
-    rules = [ ( [ ('tea leaves', 5) ], [ ('tea tree oil', 1) ] ),
-              ( [ ('primrose oil', 2), ('water', 2), ('tea tree oil', 1) ],
-                [ ( 'skin cleaning oil', 1 ) ] ),
-              ( [ ( 'footprint', 1 ) ], [ ( 'carbon', 1 ) ] ),
-              ( [ ( 'iron', 1 ), ( 'carbon', 1 ) ], [ ( 'steel', 1 ) ] ),
-              ( [ ( 'water', 4 ), ( 'mandrake root', 2 ),
-                  ( 'valerian root', 2 ), ( 'nightshade', 1 ) ],
-                [ ( 'elixir of life', 1 ) ] ),
-              ( [ ( 'ethanol', 7 ) ], [ ( 'elixir of life', 1 ) ] ),
-              ( [ ( 'chamomile', 3 ), ( 'water', 4 ), ( 'verbena', 1 ), ( 'valerian root', 1 ) ],
-                [ ( 'relaxing concoction', 1 ) ] ),
-              ( [ ( 'ice', 6 ) ], [ ( 'water', 5 ) ] ),
-              ( [ ( 'lead', 10 ), ( 'philosopher stone', 1 ), ( 'unicorn hair', 2 ) ],
-                [ ( 'gold', 10 ) ] ),
-              ( [ ( 'steel', 3 ) ], [ ( 'cable', 1 ) ] ) ]
+    rules = [([('tea leaves', 5)], [('tea tree oil', 1)]),
+             ([('primrose oil', 2), ('water', 2), ('tea tree oil', 1)],
+              [('skin cleaning oil', 1)]),
+             ([('footprint', 1)], [('carbon', 1)]),
+             ([('iron', 1), ('carbon', 1)], [('steel', 1)]),
+             ([('water', 4), ('mandrake root', 2),
+               ('valerian root', 2), ('nightshade', 1)],
+              [('elixir of life', 1)]),
+             ([('ethanol', 7)], [('elixir of life', 1)]),
+             ([('chamomile', 3), ('water', 4), ('verbena', 1), ('valerian root', 1)],
+              [('relaxing concoction', 1)]),
+             ([('ice', 6)], [('water', 5)]),
+             ([('lead', 10), ('philosopher stone', 1), ('unicorn hair', 2)],
+              [('gold', 10)]),
+             ([('steel', 3)], [('cable', 1)])]
 
-    a = alchemy( [ ( 'ethanol', 5 ) ], [ ( 'ethanol', 4 ) ], [] )
+    a = alchemy([('ethanol', 5)], [('ethanol', 4)], [])
     assert a, "avail: 5 ethanol, desired: 4 ethanol"
 
-    a = alchemy( [ ( 'tea leaves', 6 ), ( 'primrose oil', 4 ), ( 'zinc', 2 ), ( 'water', 10 ) ],
-                 [ ( 'skin cleaning oil', 1 ) ],
-                 rules )
+    a = alchemy([('tea leaves', 6), ('primrose oil', 4), ('zinc', 2), ('water', 10)],
+                [('skin cleaning oil', 1)],
+                rules)
     assert a, "skin cleaning oil, valid"
 
     # check that we have enough tea leaves after -> we don't
-    a = alchemy( [ ( 'tea leaves', 6 ), ( 'primrose oil', 4 ), ( 'zinc', 2 ), ( 'water', 10 ) ],
-                 [ ( 'skin cleaning oil', 1 ), ( 'tea leaves', 2 ) ],
-                 rules )
+    a = alchemy([('tea leaves', 6), ('primrose oil', 4), ('zinc', 2), ('water', 10)],
+                [('skin cleaning oil', 1), ('tea leaves', 2)],
+                rules)
     assert not a, "not enough tea tree leaves"
 
     # check that we have enough tea leaves after -> we do
-    a = alchemy( [ ( 'tea leaves', 6 ), ( 'primrose oil', 4 ), ( 'zinc', 2 ), ( 'water', 10 ) ],
-                 [ ( 'skin cleaning oil', 1 ), ( 'tea leaves', 1 ) ],
-                 rules )
+    a = alchemy([('tea leaves', 6), ('primrose oil', 4), ('zinc', 2), ('water', 10)],
+                [('skin cleaning oil', 1), ('tea leaves', 1)],
+                rules)
     assert a, "enough tea tree leaves"
 
     # check that we have enough tea leaves after -> we do
-    a = alchemy( [ ( 'tea leaves', 6 ), ( 'primrose oil', 4 ), ( 'zinc', 2 ), ( 'water', 10 ) ],
-                 [ ( 'tea leaves', 1 ), ( 'skin cleaning oil', 1 ) ],
-                 rules )
+    a = alchemy([('tea leaves', 6), ('primrose oil', 4), ('zinc', 2), ('water', 10)],
+                [('tea leaves', 1), ('skin cleaning oil', 1)],
+                rules)
     assert a, "enough tea tree leaves, switch order"
 
     # don't have enough steel as of now but more can be created
-    a = alchemy( [ ( 'steel', 2 ), ( 'iron', 2 ), ( 'carbon', 1 ),
-                   ( 'footprint', 2 ), ( 'water', 10 ) ],
-                 [ ( 'steel', 3 ), ( 'carbon', 2 ) ],
-                 rules )
+    a = alchemy([('steel', 2), ('iron', 2), ('carbon', 1),
+                 ('footprint', 2), ('water', 10)],
+                [('steel', 3), ('carbon', 2)],
+                rules)
     assert a, "forge steel"
 
-    a = alchemy( [ ( 'nightshade', 1 ), ( 'chamomile', 4 ),
-                   ( 'ice', 20 ), ( 'water', 2 ),
-                   ( 'verbena', 1 ), ( 'valerian root', 3 ),
-                   ( 'ethanol', 9 ), ( 'mandrake root', 2 ) ],
-                 [ ( 'elixir of life', 2 ), ( 'relaxing concoction', 1 ) ],
-                 rules )
+    a = alchemy([('nightshade', 1), ('chamomile', 4),
+                 ('ice', 20), ('water', 2),
+                 ('verbena', 1), ('valerian root', 3),
+                 ('ethanol', 9), ('mandrake root', 2)],
+                [('elixir of life', 2), ('relaxing concoction', 1)],
+                rules)
     assert a, "elixirs"
 
     # no rule to create ice
-    a = alchemy( [ ( 'ice', 1 ), ( 'water', 5 ) ],
-                 [ ( 'water', 2 ), ( 'ice', 3 ) ],
-                 rules )
+    a = alchemy([('ice', 1), ('water', 5)],
+                [('water', 2), ('ice', 3)],
+                rules)
     assert not a, "no rule to create ice"
 
-    a = alchemy( [ ( 'philosopher stone', 2 ), ( 'tea leaves', 17 ),
-                   ( 'unicorn hair', 5 ), ( 'lead', 12 ),
-                   ( 'water', 5 ) ],
-                 [ ( 'tea tree oil', 3 ), ( 'gold', 11 ) ],
-                 rules )
+    a = alchemy([('philosopher stone', 2), ('tea leaves', 17),
+                 ('unicorn hair', 5), ('lead', 12),
+                 ('water', 5)],
+                [('tea tree oil', 3), ('gold', 11)],
+                rules)
     assert not a, "no ingredients for enough gold"
 
-    a = alchemy( [ ( 'philosopher stone', 2 ), ( 'tea leaves', 17 ),
-                   ( 'unicorn hair', 5 ), ( 'lead', 22 ),
-                   ( 'water', 5 ) ],
-                 [ ( 'tea tree oil', 3 ), ( 'gold', 11 ) ],
-                 rules )
+    a = alchemy([('philosopher stone', 2), ('tea leaves', 17),
+                 ('unicorn hair', 5), ('lead', 22),
+                 ('water', 5)],
+                [('tea tree oil', 3), ('gold', 11)],
+                rules)
     assert a, "enough ingredients"
 
-    a = alchemy( [ ( 'footprint', 4 ), ( 'iron', 3 ), ( 'water', 5 ) ],
-                 [ ( 'cable', 1 ) ],
-                 rules )
+    a = alchemy([('footprint', 4), ('iron', 3), ('water', 5)],
+                [('cable', 1)],
+                rules)
     assert a, "3rd level of indirection"
 
 
